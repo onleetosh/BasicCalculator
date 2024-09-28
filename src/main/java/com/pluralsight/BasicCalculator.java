dfa68c53612f17f1edd341a98d9dd25e64b3bf52
tashi thompson <tosh@tashis-MacBook-Pro.local>
2024-09-28 12:47:18 -0400

    update

--- src/main/java/com/pluralsight/BasicCalculator.java
+++ src/main/java/com/pluralsight/BasicCalculator.java
@@ -8,43 +8,52 @@ public class BasicCalculator {
 
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter the first number " );
+        //prompt user for two values
+        System.out.print("\tEnter the first number " );
         float num1 = input.nextFloat();
-        System.out.print("Enter the second number ");
+        System.out.print("\tEnter the second number ");
         float num2 = input.nextFloat();
+        
+        //display and prompt user for an operations
+        System.out.println("Choose an operation by selecting A, S, D, or M " );
+        System.out.println("\n(A)dd");
+        System.out.println("\n(S)ubract");
+        System.out.println("\n(D)ivdide");
+        System.out.println("\n(M)ultiple");
+        String getOperation = input.next().toUpperCase();
 
-        System.out.println("Possible Calculations: " );
-        System.out.println("Add");
-        System.out.println("Subtract");
-        System.out.println("Multiply");
-        System.out.println("Divide");
-
-        System.out.print("Please select an options: ");
-        String operation = input.nextLine();
-
-        float result = 0;
-        boolean selectOperation = true;
-
-        if (operation.equals("Add")) {
+        double result = 0;
+        boolean operation = true;
+        
+        //perform addition 
+        if (getOperation.equals("A")) {
             result = num1 + num2;
-            System.out.println("Result " + result);
-        }
-
-        else if (operation.equals("Subtract")){
-            result = num1 + num2;
-            System.out.println("Result: " + result);
-        } else if (operation.equals("Divide")) {
-            result = num1 / num2;
-            System.out.println("Result " + result);
-        } else if (operation.equals("Multiple")) {
+            System.out.println("\tResult " + num1 + " + " + num2 + " = " + result);
+        } 
+        //perform substraction 
+        else if (getOperation.equals("S")) {
+            result = num1 - num2;
+            System.out.println("\tResult " + num1 + " - " + num2 + " = " + result);
+        } 
+        //perform multiplication
+        else if (getOperation.equals("M")) {
             result = num1 * num2;
-            System.out.println("Result " + result);
+            System.out.println("\tResult " + num1 + " * " + num2 + " = " + result);
+        } 
+        //perform division 
+        else if (getOperation.equals("D")) {
+            if (num2 != 0) {
+                result = num1 / num2;
+                System.out.println("\tResult " + num1 + " / " + num2 + " = " + result);
+            } else {
+                System.out.println("\tError: number enter cannot divide by zero.");
+            }
+        } else {
+            operation = false;
+            System.out.println("\tError: not an operation.");
         }
-        else {
-            selectOperation = false;
-            System.out.println("Error " );
-        }
-
-
+        
     }
+
+
 }
